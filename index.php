ed9a4e439899c2609836c356818e99d2dfb4c27c
Alex Fox <alexanderbenfox@gmail.com>
2016-03-14 19:10:04 -0400

    Code's pretty clean for the most part - I'm sure there's some unnecessary css in there but I think it's probably fine enough.  It looks good but I still can improve and I think that's a good thing.

--- index.php
+++ index.php
@@ -55,19 +55,6 @@
 			  text-align:center;
 			}
 
-			.bubble-speech.bubble-center:after{
-			    z-index: 9002;
-			display: block;
-			content: "";
-			position:absolute;
-			bottom:-12px;
-			left: 50%;
-			margin-left:-18px;
-			border-right: 20px transparent solid;
-			border-top: 12px #efefef solid;
-			border-left: 20px  transparent solid;
-			}
-
 			.bubble-speech.bubble-right:after{
 			  z-index: 9002;
 			display: block;
@@ -90,10 +77,6 @@
 			.t-image {
 			    position: relative; /* Declared position allows for location changes */
 			    top: -50px; /* Moves the image 2px closer to the top of the page */
-			}
-			.t-image2 {
-			    position: relative; /* Declared position allows for location changes */
-			    top: -70px; /* Moves the image 2px closer to the top of the page */
 			}
 			.flip-img{
 				position: relative;
@@ -130,62 +113,13 @@
 				float:right;
 				overflow: scroll;
 			}
-			#blog{
-
-			}
+			#blog{}
 		</style>
-		<script>
-			$(document).ready(function(){
-				$(window).bind('scroll',function(e){
-			   		parallaxScroll();
-			   	});
-			   	
-			   	
-			   	function parallaxScroll(){
-			   		var body = document.body,
-    					html = document.documentElement;
-
-					var height = Math.max(	body.scrollHeight, body.offsetHeight, 
-                       						html.clientHeight, html.scrollHeight, html.offsetHeight );
-					var backgroundscroll = .1;
-						closemountainscroll = .5;
-						farmountainscroll = .7;
-						mountaintreesscroll = .3;
-						mountaintreesforegroundscroll = .2;
-			   		var scrolledY = $(window).scrollTop();
-					$('.bgWrapper').css('background-position','center -'+((scrolledY*backgroundscroll))+'px');
-					$('.closemountain').css('top','-'+((scrolledY*closemountainscroll))+'px');
-					$('.farmountain').css('top','-'+((scrolledY*farmountainscroll))+'px');
-					$('.mountaintrees').css('top','-'+((scrolledY*mountaintreesscroll))+'px');
-					$('.mountaintreesforeground').css('top','-'+((scrolledY*mountaintreesforegroundscroll))+'px');
-					
-					
-			   	}
-			   	function getCount(parent, getChildrensChildren){
-				    var relevantChildren = 0;
-				    var children = parent.childNodes.length;
-				    for(var i=0; i < children; i++){
-				        if(parent.childNodes[i].nodeType != 3){
-				            if(getChildrensChildren)
-				                relevantChildren += getCount(parent.childNodes[i],true);
-				            relevantChildren++;
-				        }
-				    }
-				    return relevantChildren;
-				}
-
-			   	
-			});
-		</script>
 	</head>
 	<body>
 		<div class="bg"></div>
 		<div class="mainWrapper">
 			<h1>alexfoxgames</h1>
-
-		
-			
-
 			<p class = "rel">	
 				<div class="bubble-speech bubble-left">
 					<div class="message" style = "text-align: center">
@@ -205,14 +139,14 @@
 			<p class = "rel">	
 				<div class="bubble-speech bubble-right" style = "position:relative;top:-17px;">
 					<div style = "text-align: center">
-						<a style = "text-align :center" href="http://localhost/jq/spacegame.html">Warp Hunter</a>
+						<a style = "position:relative;text-align:center" href="http://localhost/jq/spacegame.html">Warp Hunter</a>
 					</div>
 				</div>
 				<div class = "flip-img">
 					<a target="_blank" href="darkgame/merchant.gif">
 				    	<img src="darkgame/merchant.gif" alt="space" align = "left">
 					</a>
-					<p class = "flip-img" style = "position:relative;top:20px;left:10px">
+					<p class = "flip-img" style = "position:relative;top:20px;left:30px;margin-left:60px; margin-right:120px">
 						A game that borrows lovingly from it's inspiration: Super Metroid.
 					</p>
 				</div>
@@ -224,11 +158,11 @@
 						<a href="http://localhost/jq/namelessgame.html">Harbinger</a>
 					</div>
 				</div>
-				<div class = "t-image" style = "position:relative;right:-10px;margin:auto;">
+				<div class = "t-image" style = "position:relative;top:-35px;right:-20px;margin:auto;">
 					<a target="_blank" href="darkgame/01.png">
 				    	<img src="darkgame/01.png" alt="space" align = "left">
 					</a>
-					<p style = "position:relative;top:30px;">
+					<p style = "position:relative;top:20px;left:30px; margin-left:120px;margin-right:60px">
 						Look at my current project: a game about slaying Demons.
 					</p>
 				</div>
@@ -247,92 +181,88 @@
 
       		</div>
 
+<!--       		BEGIN DATABASE SECTION -->
+			<?
+				if(isset($_GET['name'])){
+					$name = $_GET['name'];
+					$email = $_GET['email'];
+					$fav_game = $_GET['fav_game'];
 
-<?
-	if(isset($_GET['name'])){
-		$name = $_GET['name'];
-		$email = $_GET['email'];
-		$fav_game = $_GET['fav_game'];
+					if(!$_GET['name']){
+				 		exit();
+					}else{
+						$host = "localhost";
+						$user = "root";
+						$pass = "";
+						$link = mysql_connect($host, $user, $pass);
+						mysql_select_db("test",$link);
 
-		if(!$_GET['name']){
-	 		exit();
-		}else{
-			$host = "localhost";
-			$user = "root";
-			$pass = "";
-			$link = mysql_connect($host, $user, $pass);
-			mysql_select_db("test",$link);
+						$query_name = "SELECT name FROM mailinglist_tbl WHERE " . 'name' ." = ".'"'."$name".'"'.";";
+						$query_email = "SELECT email FROM mailinglist_tbl WHERE " . 'email' ." = ".'"'."$email".'"'.";";
+						$query_game = "SELECT fav_game FROM mailinglist_tbl WHERE " . 'fav_game' ." = ".'"'."$fav_game".'"'.";";
 
-			$query_name = "SELECT name FROM mailinglist_tbl WHERE " . 'name' ." = ".'"'."$name".'"'.";";
-			$query_email = "SELECT email FROM mailinglist_tbl WHERE " . 'email' ." = ".'"'."$email".'"'.";";
-			$query_game = "SELECT fav_game FROM mailinglist_tbl WHERE " . 'fav_game' ." = ".'"'."$fav_game".'"'.";";
+						$name_result = mysql_query($query_name) or die(mysql_error());
+						$email_result = mysql_query($query_email) or die(mysql_error());
+						$game_result = mysql_query($query_game) or die(mysql_error());
 
-			$name_result = mysql_query($query_name) or die(mysql_error());
-			$email_result = mysql_query($query_email) or die(mysql_error());
-			$game_result = mysql_query($query_game) or die(mysql_error());
+						$thenum = mysql_num_rows($name_result);
+						echo '<p style = "margin-top:20px">';
 
-			$thenum = mysql_num_rows($name_result);
+						if(mysql_num_rows($name_result) == 0){
+							//row wasn't found - we can add it
+							$command = "INSERT INTO mailinglist_tbl (name, email, fav_game) VALUES (".'"'."$name".'",'.'"'."$email".'",'.'"'."$fav_game".'"'.");";
+							echo "Name successfully added to mailing list!";
+							mysql_query($command, $link)or die(mysql_error());
 
-			if(mysql_num_rows($name_result) == 0){
-				//row wasn't found - we can add it
-				$command = "INSERT INTO mailinglist_tbl (name, email, fav_game) VALUES (".'"'."$name".'",'.'"'."$email".'",'.'"'."$fav_game".'"'.");";
-				echo "Name successfully added to mailing list!";
-				mysql_query($command, $link)or die(mysql_error());
+						} else {
+							echo "Could not add name to mailing list!";
+							//row was found - don't add it
 
-			} else {
-				echo "Could not add name to mailing list!";
-				//row was found - don't add it
+						}
+						echo '</p>';
 
-			}
-
-			$query = "SELECT * FROM mailinglist_tbl";$result = mysql_query($query)or die(mysql_error());
-			$num = mysql_num_rows($result);
-?>
-			<table border = "1" align = "center">
-							<tr align = "center">
-         	<td>
-         </td>
-         <td> NAME </td>
-         <td> EMAIL </td> 
-         <td> FAVORITE GAME </td> 
-         </tr>
-<?
-			$array = array();
-			for($i = 0; $i<$num; $i++){
-				$show_name = mysql_result($result,$i,"name")or die(mysql_error());
-				$show_email = mysql_result($result,$i,"email")or die(mysql_error());
-				$show_fav_game = mysql_result($result,$i,"fav_game")or die(mysql_error());
-?>
-			<tr align = "center">
-         	<td>
-         </td>
-         <td> <?php echo $show_name;?> </td>
-         <td> <?php echo $show_email;?> </td> 
-         <td> <?php echo $show_fav_game;?> </td> 
-         </tr>
-<?
-							//array_push($array, $show_lname, $show_fname);
-			}
+						$query = "SELECT * FROM mailinglist_tbl";$result = mysql_query($query)or die(mysql_error());
+						$num = mysql_num_rows($result);
 			?>
-		</table>
+			<table style = "margin-top:20px"border = "1" align = "center">
+				<tr align = "center">
+			        <td style = "font-size:x-large"> NAME </td>
+			        <td style = "font-size:x-large"> EMAIL </td> 
+			        <td style = "font-size:x-large"> FAVORITE GAME </td> 
+		        </tr>
+			<?
+				$array = array();
+				for($i = 0; $i<$num; $i++){
+					$show_name = mysql_result($result,$i,"name")or die(mysql_error());
+					$show_email = mysql_result($result,$i,"email")or die(mysql_error());
+					$show_fav_game = mysql_result($result,$i,"fav_game")or die(mysql_error());
+			?>
+				<tr align = "center">
+         			<td> <?php echo $show_name;?> </td>
+        			<td> <?php echo $show_email;?> </td> 
+         			<td> <?php echo $show_fav_game;?> </td> 
+         		</tr>
+			<?
+				}
+			?>
+			</table>
 			<?
 
 
-			mysql_close($link);
-		}
-	}
-		
-
-?>
-
-
-
-			
+					mysql_close($link);
+				}
+			}
+			?>
 		</div>
 		<div class = "blogWrapper">
 			<h1 id = "blog" >Development Blog</h1>
 				<script type="text/javascript">
-					var posts = ["Check back here for the latest development news!  Will be adding a comment feature to this devblog wall very soon but I do not have time right now!", "Test Post", "javascript is really really cool!"];
+					<?
+						$posts = file("blogposts.txt");
+						$js_array = json_encode($posts);
+						echo "var posts = ". $js_array . ";\n"
+					?>
+					//var posts = ["Check back here for the latest development news!  Will be adding a comment feature to this devblog wall very soon but I do not have time right now!", "Test Post", "javascript is really really cool!"];
 					var iDiv = document.createElement('div');
 					//iDiv.id = 'block';
 
