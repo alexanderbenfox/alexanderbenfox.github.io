ab6f11e039e76ba34a84bcfe24fee3263f0afd47
Alex Fox <alexanderbenfox@gmail.com>
2017-04-04 09:46:10 -0400

    added blog lol

--- index/js/blog.js
+++ index/js/blog.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
 
-	$.ajax({
+	/*$.ajax({
 	    url:"../php/getBlogPosts.php",
 	    type:"POST",
 	    success:function(stuff){
 	        createPosts(stuff);
 	    },
 	    dataType:"json"
-	});
+	});*/
 
-	function createPosts(posts){
+	function Post(title, date, words, link){
+		this.title = title;
+		this.date = date;
+		this.words = words;
+		this.link = link;
+
+	}
+
+	var posts = [];
+
+	var firstPost = new Post("Some music", "4/4/2017", "No blog posts yet, so here's a song that I like", "https://www.youtube.com/embed/fYSMZpiF7sA" )
+	posts.push(firstPost);
+	//function createPosts(posts){
 		var iDiv = document.createElement('div');
 
 		for (var i = 0; i < posts.length; i++) {
@@ -21,14 +33,26 @@ $(document).ready(function(){
 
 			var authorDiv = document.createElement('div');
 			authorDiv.className = "author";
-			authorDiv.style.fontSize = 'medium'
-			authorDiv.innerHTML = "3/14/2016";
+			authorDiv.style.fontSize = 'medium';
+			authorDiv.innerHTML = posts[i].date;
 
 			var messageDiv = document.createElement('div');
 			messageDiv.className = "message";
 			messageDiv.style.textAlign = "left";
 			messageDiv.style.fontSize = 'small';
-			messageDiv.innerHTML = posts[i];
+
+			var more = "";
+			if(posts[i].link != null){
+				var text = '<iframe width="250" height="200" src="QQQQ?autohide=0" frameborder="0" allowfullscreen></iframe>';
+				text = text.replace("QQQQ",posts[i].link);
+				more += text;
+			}
+
+			var anotherDiv = document.createElement('div');
+			anotherDiv.innerHTML = more;
+
+			messageDiv.innerHTML = posts[i].words;
+			messageDiv.appendChild(anotherDiv);
 
 			innerDiv.appendChild(authorDiv);
 			innerDiv.appendChild(messageDiv);
@@ -37,5 +61,5 @@ $(document).ready(function(){
 		}
 
 		document.getElementById('blog').appendChild(iDiv);
-	}
+	//}
 });
