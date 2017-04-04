c9fa947a0c384269b6e50ffba42c9ae47938a3f5
Alex Fox <alexanderbenfox@gmail.com>
2017-04-04 05:59:28 -0400

    removing php

---
+++ index/html/index.php
@@ -0,0 +1,79 @@
+
+
+<html>
+	<head>
+		<title>ALEXFOXGAMES Main Page</title>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+		<link rel="stylesheet" href= "../../general_use/css/parallaxelements.css" type="text/css" charset="utf-8"/>
+		<link rel="stylesheet" href="../../general_use/css/font_ss.css" type="text/css" charset="utf-8" />
+		<link rel="stylesheet" href="../css/index_ss.css" type="text/css" charset="utf-8"/>
+	</head>
+	<body>
+		<div class="bg"></div>
+		<div class="mainWrapper">
+			<h1>alexfoxgames</h1>
+			<p class = "rel">	
+				<div class="bubble-speech bubble-left">
+					<div class="message" style = "text-align: center">
+						<a href="../../ehb_page/html/ehb.html">EHB/Project Torchman</a>
+					</div>
+				</div>
+				<div class = "t-image">
+					<a target="_blank" href="../../ehb_page/assets/ehb1.jpg">
+						<img src="../../ehb_page/assets/ehb1.jpg" alt="space" align = "left">
+					</a>
+					<p style = "position:relative;top:30px;">
+						Get to learn a little about my first project.
+					</p>
+				</div>
+			</p>
+
+			<p class = "rel">	
+				<div class="bubble-speech bubble-right" style = "position:relative;top:-17px;">
+					<div style = "text-align: center">
+						<a style = "position:relative;text-align:center" href="../../warp_hunters_page/html/spacegame.html">Warp Hunter</a>
+					</div>
+				</div>
+				<div class = "flip-img">
+					<a target="_blank" href="../../harbinger_page/assets/merchant.gif">
+						<img src="../../harbinger_page/assets/merchant.gif" alt="space" align = "left">
+					</a>
+					<p class = "flip-img" style = "position:relative;top:20px;left:30px;margin-left:60px; margin-right:120px">
+						A game that borrows lovingly from it's inspiration: Super Metroid.
+					</p>
+				</div>
+			</p>
+
+			<p class = "rel">	
+				<div class="bubble-speech bubble-left" style = "position:relative;top:-10px;">
+					<div class="message" style = "text-align: center">
+						<a href="../../harbinger_page/html/namelessgame.html">Harbinger</a>
+					</div>
+				</div>
+				<div class = "t-image" style = "position:relative;top:-35px;right:-20px;margin:auto;">
+					<a target="_blank" href="../../harbinger_page/assets/01.png">
+						<img src="../../harbinger_page/assets/01.png" alt="space" align = "left">
+					</a>
+					<p style = "position:relative;top:20px;left:30px; margin-left:120px;margin-right:60px">
+						Look at my current project: a game about slaying Demons.
+					</p>
+				</div>
+			</p>
+		</div>
+		<div class = "blogWrapper">
+			<h1 id = "blog" >Development Blog</h1>
+			<script type="text/javascript" src="../js/blog.js"></script>
+		</div>
+
+		<div class = "commentsWrapper">
+			<script type="text/javascript" src="../js/comments.js"></script>
+			<h1>Leave a comment!</h1>
+				<div id ="commentBox">
+					<form>
+					</form>
+				</div>
+				<div id ="commentWall"></div>
+			<script type="text/javascript" src="../js/blog.js"></script>
+		</div>
+	</body>
+</html>
